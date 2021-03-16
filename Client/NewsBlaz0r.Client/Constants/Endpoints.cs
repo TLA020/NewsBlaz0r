3c54959a7fed4e69fbae0fe1eefd36c72a3cc482
Tobias <t.lie-atjam@effytool.nl>
2021-03-16 19:04:29 +0100

    add dropdown to switch endpoint

--- Client/NewsBlaz0r.Client/Constants/Endpoints.cs
+++ Client/NewsBlaz0r.Client/Constants/Endpoints.cs
@@ -3,7 +3,12 @@
     public class Endpoints
     {
         public const string AdNews = "https://www.ad.nl/nieuws/rss.xml";
+        public const string AdMisdaad = "https://rss.at5.nl/rss/misdaad";
+        public const string Ad112 = "https://rss.at5.nl/rss/112";
+        
+        // emergencies
         public const string Police = "https://alarmeringen.nl/feeds/discipline/politie.rss";
         public const string RegioNoordHolland = "https://alarmeringen.nl/feeds/region/noord-holland-noord.rss";
+
     }
 }
